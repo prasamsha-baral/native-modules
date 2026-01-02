92871c08a9b806a40952f6f9050b71d1ec5599b3
prasamsha baral <baralprasamsha@gmail.com>
2026-01-02 08:13:51 +0545

    read file

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const fs=require("fs")
 
-fs.writeFile("message.txt","hello from nodejs",(err)=>{
-    if (err)throw err;
-    console.log("the file has been saved")
-})
-fs.readFile("message.txt", (err,data)=>{
-    if(err)throw err;
-    console.log(data)
-})
+// fs.writeFile("message.txt","hello from nodejs",(err)=>{
+//     if (err)throw err;
+//     console.log("the file has been saved1")
+// })
+
+
+//it outputs the data in encoded form
+
+// fs.readFile("message.txt", (err,data)=>{
+//     if(err)throw err;
+//     console.log(data)
+// })
+
+//utf8 is used to output the data in readable form 
+//we have to comment out the writefile so that it doesnt overwrite 
+
 fs.readFile("message.txt","utf8",(err,data)=>{
     console.log(data)
 }
 )
+
