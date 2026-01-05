a358539353699d0ddd3dc425354515d9165abbd4
prasamsha baral <baralprasamsha@gmail.com>
2026-01-05 22:24:08 +0545

    modified

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const fs=require("fs")
+// const { inherits } = require("util")
 
-// fs.writeFile("message.txt","hello from nodejs",(err)=>{
-//     if (err)throw err;
-//     console.log("the file has been saved1")
-// })
+fs.writeFile("message.txt","hello from nodejs",(err)=>{
+    if (err)throw err;
+    console.log("the file has been saved1")
+
+fs.appendFile("message.txt", "\nNew line added", (err) => {
+    if (err) throw err;
+    console.log("Data appended");
+
+
+
+//utf8 is used to output the data in readable form 
+//we have to comment out the writefile so that it doesnt overwrite 
+
+fs.readFile("message.txt","utf8",(err,data)=>{
+    if(err){
+        console.log("error reading fie:",err);
+        return;
+    }
+    console.log(data)
+});
+
+});
+
+});
+
+// fs.rename("message.txt", "data.txt", (err) => {
+//     if (err) throw err;
+//     console.log("File renamed");
+// });
 
 
 //it outputs the data in encoded form
@@ -13,11 +39,8 @@ const fs=require("fs")
 //     console.log(data)
 // })
 
-//utf8 is used to output the data in readable form 
-//we have to comment out the writefile so that it doesnt overwrite 
 
-fs.readFile("message.txt","utf8",(err,data)=>{
-    console.log(data)
-}
-)
+
+
+// npm init
 
